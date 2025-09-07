0c6965d7912e43c1926b86f0c3d90668db40440c
Dhanavarshini.V <126056656+DHANAVARSHINIV@users.noreply.github.com>
2025-09-07 11:52:33 +0530

    Create email_sender.py

---
+++ travel_agent/email_sender.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.mime.text import MIMEText
+from config import EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS
+
+def send_email(to_email, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = EMAIL_USER
+    msg['To'] = to_email
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASS)
+        server.send_message(msg)
+  
